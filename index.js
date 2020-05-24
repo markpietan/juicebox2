db0bf89804a5a18eafe2f3287b38471a07c96fd7
Your Name <your_email@real_email.edu>
2020-05-24 13:46:10 -0400

    finishing up

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const PORT = 3000;
-const express = require('express');
+const { PORT = 3000 } = process.env;
+const express = require("express");
 const server = express();
-const apiRouter = require('./api');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { client } = require('./db');
-require('dotenv').config();
-server.use(morgan('dev'));
+const apiRouter = require("./api");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { client } = require("./db");
+require("dotenv").config();
+server.use(morgan("dev"));
 server.use(bodyParser.json());
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
 
 client.connect();
 
-
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-  server.get('/background/:color', (req, res, next) => {
-    res.send(`
-      <body style="background: ${ req.params.color };">
+  next();
+});
+
+server.get("/background/:color", (req, res, next) => {
+  res.send(`
+      <body style="background: ${req.params.color};">
         <h1>Hello World</h1>
       </body>
     `);
-  });
+});
 
-  server.get('/add/:first/to/:second', (req, res, next) => {
-    res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+server.get("/add/:first/to/:second", (req, res, next) => {
+  res.send(
+    `<h1>${req.params.first} + ${req.params.second} = ${
       Number(req.params.first) + Number(req.params.second)
-     }</h1>`);
-  });
+    }</h1>`
+  );
+});
 
-server.use('/api', apiRouter);
-  
+server.use("/api", apiRouter);
